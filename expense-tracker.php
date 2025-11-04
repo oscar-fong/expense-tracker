c357b2f5649d6fd0ff95f8a76333e9519601b051
Oscar Fong <oscarfongfuentes@gmail.com>
2025-11-04 06:09:09 -0700

    Update .gitignore to allow config json files and ignore data json files and Update expense-tracker.php that uses a console library to make php CLI applications, in the next commit I might do the program manually without libraries to avoid overcomplexity and to show logic

--- expense-tracker.php
+++ expense-tracker.php
@@ -1,1 +1,43 @@
+#!/usr/bin/php
 <?php
+require __DIR__ . '/vendor/autoload.php';
+
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Command\Command;
+
+// ->addArgument('username', InputArgument::REQUIRED)
+$application = new Application;
+$application->register('add')
+  ->setDescription('Add an expense')
+  ->addOption(
+    'description',
+    'd',
+    InputOption::VALUE_REQUIRED,
+    'Description of the product bought',
+  )
+  ->addOption(
+    'amount',
+    'a',
+    InputOption::VALUE_REQUIRED,
+    'How much did it cost',
+  )
+  ->setCode(function (InputInterface $input, OutputInterface $output): int {
+    $description = $input->getOption('description');
+    $amount = $input->getOption('amount');
+    if ($description && $amount) {
+      $output->writeln($amount);
+    }
+    return Command::SUCCESS;
+    //$output->writeln()
+
+    //return $output->getFormatter()
+  });
+
+//$application->register('')
+
+
+$application->run();
